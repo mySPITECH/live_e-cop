a719bb513911671da84df08d502600f7dbbc48b3
myspitec <samuelsamafolabi@gmail.com>
2018-09-07 17:32:56 +0100

    Added files and changes required for deployment to heroku

--- coop/tables.py
+++ coop/tables.py
@@ -1,9 +1,13 @@
 import django_tables2 as tables
-from .models import UserInfo
+from .models import UserInfo,UserAccount
 
 class UserTable(tables.Table):
     class META:
         model=UserInfo
         template_name= 'django_tables2/bootstrap.html'
-    
+
+class UserAccountTable(tables.Table):
+    class META:
+        model=UserAccount
+        template_name = 'django_tables2/bootstrap.html'
         
